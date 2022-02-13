42bb5e83a6b50a48377febed51fa60325f9869c3
Paules2021 <P.elishaee@gmail.com>
2022-02-13 06:32:43 +0000

    add helper and user.js routing

---
+++ helpers.js
@@ -0,0 +1,32 @@
+
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'labber',
+  password: 'labber',
+  host: 'localhost',
+  database: 'midterm'
+});
+
+const addUser = function (user){
+  return pool
+  .querry(`INSERT INTO users(name,email,password)
+  VALUES($1, $2, $3)
+  RETURNING *`
+  ,[user.name, user.email, user.password])
+  .then(res=>res.rows[0])
+}
+exports.addUser=addUser
+
+
+const login = function(email, password) {
+  return pool.query(`
+  SELECT * FROM users
+  WHERE email = $1;
+  `, [email])
+  .then (res => {
+    const user = res.rows[0];
+    return user.password === password ? user : null;
+  })
+}
+exports.login = login;
--- routes/users.js
+++ routes/users.js
@@ -6,7 +6,8 @@
  */
 
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
+const helper = require('../helper');
 
 module.exports = (db) => {
   router.get("/", (req, res) => {
@@ -21,5 +22,79 @@ module.exports = (db) => {
           .json({ error: err.message });
       });
   });
+
+
+  // users/new GET - goes to registration page
+
+  router.get("/new", (req, res) => {
+    //if user already loged in redirect home
+    if (req.session.user_id) {
+      res.redirect('../')
+    }
+    const templeVars = { user_id: req.session.user_id }
+    res.render("user_login", templeVars)
+
+  })
+
+
+  // user/login GET -  Login page
+
+  router.get("/login", (req, res) => {
+    // if user is already logged in
+    if (req.session.user_id) {
+      res.redirect('../');
+    }
+    const templateVars = { user_id: req.session.user_id };
+    res.render("user_login", templateVars);
+  });
+
+
+  // users/new POST : create new account, add user to db, redirect to home
+
+  router.post("/", (req, res) => {
+    const user = req.body
+    helper.addUser(user)
+      .then(user => {
+        if (!user) {
+          res.send({ error: 'ERORR : User/Password not found!' })
+          return;
+        }
+        //cookie assigned with user_id for user
+        req.session.user_id = user.id;
+        // redirects to user page
+        res.redirect(`/users/${user.id}`);
+      })
+      .catch(e => res.send(e));
+  });
+
+
+  // users/login POST  Login update session cookie
+
+  router.post("/login", (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+    helper.login(email, password)
+      .then(user => {
+        if (!user) {
+          res.send({ error: 'ERROR : username/password is incorrect!' });
+          return;
+        }
+        // cookie assigned with user_id
+        req.session.user_id = user.id;
+        // redirects to user page
+        res.redirect(`/users/${user.id}`);
+      })
+      .catch(e => res.send(e));
+  });
+
+
+
+
+
+
+
+
   return router;
 };
+
+
