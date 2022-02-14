b7074a3bebd8b169ab1ede931859595fc7cb2efe
mikiyoshi <mikiyoshi.kokura@gmail.com>
2022-02-14 20:08:16 +0000

    add cookie

--- routes/login.js
+++ routes/login.js
@@ -19,6 +19,8 @@ module.exports = (db) => {
         let users = data.rows;
         console.log('GETtemplate', users[0]);
         const templateVars = users[0];
+        let loginId = users[0].id;
+        req.session['user_id'] = loginId;
         res.render('index', templateVars);
       })
       .catch((err) => {
