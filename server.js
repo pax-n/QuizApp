6fd30db0feb64ec5b3d68494c5854b47987e6722
mikiyoshi <mikiyoshi.kokura@gmail.com>
2022-02-15 04:11:03 +0000

    update login with cookie session status

--- server.js
+++ server.js
@@ -63,7 +63,7 @@ const quizzesRoutes = require('./routes/quizzes');
 const questionsRoutes = require('./routes/questions');
 const answersRoutes = require('./routes/answers');
 const attemptsRoutes = require('./routes/attempts');
-const loginRoutes = require('./routes/login');
+// const loginRoutes = require('./routes/login');
 // const loginPostRoutes = require('./routes/login-post');
 const quizpageRoutes = require('./routes/quizpage');
 const resultpageRoutes = require('./routes/resultpage');
@@ -72,6 +72,7 @@ const userRoutes = require('./routes/user');
 const registerRoutes = require('./routes/register');
 const resetRoutes = require('./routes/reset');
 const errorRoutes = require('./routes/error');
+const homeQuizRoutes = require('./routes/homequiz');
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
@@ -81,7 +82,7 @@ app.use('/api/quizzes', quizzesRoutes(db));
 app.use('/api/questions', questionsRoutes(db));
 app.use('/api/answers', answersRoutes(db));
 app.use('/api/attempts', attemptsRoutes(db));
-app.use('/login', loginRoutes(db));
+// app.use('/login', loginRoutes(db));
 // app.use('/login-post', urlencodedParser, loginPostRoutes(db));
 app.use('/quizpage', quizpageRoutes(db));
 app.use('/resultpage', resultpageRoutes(db));
@@ -96,17 +97,15 @@ app.use('/error', errorRoutes(db));
 // Warning: avoid creating more routes in this file!
 // Separate them into separate routes files (see above).
 
-app.get('/', (req, res) => {
-  console.log(req.session['user_id']);
-  res.render('index');
-});
+// app.get('/', (req, res) => {
+//   console.log(req.session['user_id']);
+//   res.render('index');
+// });
+// Homepage receive all quiz routes
+app.use('/', homeQuizRoutes(db));
 
 app.get('/demo_index', (req, res) => {
   res.render('demo_index');
-});
-
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}`);
 });
 
 // User cookie session
@@ -116,5 +115,36 @@ app.get('/login/:id', (req, res) => {
   // cookie-parser middleware
   res.cookie('user_id', req.params.id);
   // send the user somewhere
-  res.redirect('/');
+  let user_id = req.params.id;
+  //
+  let query = `SELECT * FROM users`;
+  db.query(query)
+    .then((data) => {
+      console.log(data);
+      let users = data.rows;
+      console.log('GETtemplate', users[user_id]);
+      const templateVars = {
+        user_id: user_id,
+        user: users[user_id],
+      };
+      res.render('index', templateVars);
+    })
+    .catch((err) => {
+      res.status(500).json({ error: err.message });
+    });
+});
+
+//
+// POST /logout
+//
+app.post('/logout', (req, res) => {
+  req.session.user_id = null;
+  const templateVars = {
+    user: undefined,
+  };
+  res.render('index', templateVars);
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`);
 });
