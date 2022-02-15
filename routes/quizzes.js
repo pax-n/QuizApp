890c903136171d9c192a2544cb5815efbd84204f
Paules2021 <P.elishaee@gmail.com>
2022-02-15 15:54:37 +0000

    merge

--- routes/quizzes.js
+++ routes/quizzes.js
@@ -3,11 +3,10 @@
  * Since this file is loaded in server.js into api/quizzes,
  *   these routes are mounted onto /quizzes
  * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
-
+ */
 
 const express = require('express');
 const router = express.Router();
-
 module.exports = (db) => {
   router.get('/', (req, res) => {
     let query = `SELECT * FROM quizzes`;
