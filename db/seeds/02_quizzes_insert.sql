b0e95096b972e326883790d739824b3eba39e62d
Paules2021 <P.elishaee@gmail.com>
2022-02-18 15:35:14 +0000

    update database

--- db/seeds/02_quizzes_insert.sql
+++ db/seeds/02_quizzes_insert.sql
@@ -1,4 +1,4 @@
 INSERT INTO quizzes (user_id, name, description, isPrivate) VALUES(1, 'Culture', 'Origin of the musical instruments', FALSE);
-INSERT INTO quizzes (user_id, name, description, isPrivate) VALUES(2, 'Geography', 'Capitals of the countries', FALSE);
+INSERT INTO quizzes (user_id, name, description, isPrivate) VALUES(2, 'Geography', 'Do you Know Capitals of the countries?', FALSE);
 INSERT INTO quizzes (user_id, name, description, isPrivate) VALUES(3, 'Sport', 'Champions of the soccer world cup', FALSE);
 INSERT INTO quizzes (user_id, name, description, isPrivate) VALUES(4, 'History', 'Important Events in Canadian history', FALSE);
